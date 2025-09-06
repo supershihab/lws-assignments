86ad092b873f5597635534a0c01d6d6685c1dc90
Shihab M <61880103+supershihab@users.noreply.github.com>
2025-09-06 23:19:08 +0600

    feat: Add Header and Hero section

--- 01-landing-page/src/App.jsx
+++ 01-landing-page/src/App.jsx
@@ -1,8 +1,14 @@
-import './App.css'
+import "./App.css";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 function App() {
-
-  return <h1 className="text-3xl font-bold underline">Hello World!</h1>;
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ 01-landing-page/src/components/logos/Figma.jsx
@@ -0,0 +1,42 @@
+const Figma = () => {
+  return (
+    <svg
+      className="w-4 h-4 mr-2"
+      id="Layer_1"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 200 300"
+      width="1667"
+      height="2500"
+    >
+      <title>Figma.logo</title>
+      <desc>Created using Figma</desc>
+      <path
+        id="path0_fill"
+        style={{ fill: "#0acf83" }}
+        d="M50 300c27.6 0 50-22.4 50-50v-50H50c-27.6 0-50 22.4-50 50s22.4 50 50 50z"
+      />
+      <path
+        id="path1_fill"
+        style={{ fill: "#a259ff" }}
+        d="M0 150c0-27.6 22.4-50 50-50h50v100H50c-27.6 0-50-22.4-50-50z"
+      />
+      <path
+        id="path1_fill_1_"
+        style={{ fill: "#f24e1e" }}
+        d="M0 50C0 22.4 22.4 0 50 0h50v100H50C22.4 100 0 77.6 0 50z"
+      />
+      <path
+        id="path2_fill"
+        style={{ fill: "#ff7262" }}
+        d="M100 0h50c27.6 0 50 22.4 50 50s-22.4 50-50 50h-50V0z"
+      />
+      <path
+        id="path3_fill"
+        style={{ fill: "#1abcfe" }}
+        d="M200 150c0 27.6-22.4 50-50 50s-50-22.4-50-50 22.4-50 50-50 50 22.4 50 50z"
+      />
+    </svg>
+  );
+};
+
+export default Figma;
